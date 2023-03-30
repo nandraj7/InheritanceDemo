b59173e21f91526dd5a73a0cf9c515613fa19308
hp <hp@HPELITEBOOK840G>
2023-03-30 23:13:57 +0530

    Add project files.

---
+++ Demo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    internal class Demo
+    {
+        internal class GrandParent
+        {
+            public void Function1()
+            { 
+            
+            }
+            public void Function2()
+            {
+
+            }
+
+        }
+        internal class Parent : GrandParent
+        {
+            public void Function3()
+            {
+
+            }
+            public void Function4()
+            {
+
+            }
+        }
+        internal class Child1 : Parent
+        {
+            public void Function5()
+            {
+
+            }
+            public void Function6()
+            {
+
+            }
+        }
+        internal class Child2 : Parent
+        {
+            public void Function7()
+            {
+            }
+            public void Function8()
+            {
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using InheritanceDemo;
+using static InheritanceDemo.Demo;
+
+Child1 c1 = new Child1();
+c1.Function1();
+c1.Function2();
+c1.Function3();
+c1.Function4();
+c1.Function5();
+c1.Function6();
+//c2.Function7();
+//c2.Function8();
+
+
+Child2 c2 = new Child2();
+c2.Function1();
+c2.Function2();
+c2.Function3();
+c2.Function4();
+//c2.Function5();
+//c2.Function6();
+c2.Function7();
+c2.Function8();
+
+
+Parent p = new Parent();
+p.Function1();
+p.Function2();
+p.Function3();
+p.Function4();
+
+
+
+GrandParent gp = new GrandParent();
+gp.Function1();
+gp.Function2();
+
+
